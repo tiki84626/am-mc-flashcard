db4d499397fcfc70ff56fd650fdf8871fd68381d
Andrew Malkin and Martin Cartledge <martin@>
2016-08-09 15:13:16 -0700

    add flashcards, pointer, styling, and place to put answers for our flashcards

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $('#showdiv1').click(function() {
+        $('div[id^=div]').hide();
+        $('#div1').show();
+    });
+    $('#showdiv2').click(function() {
+        $('div[id^=div]').hide();
+        $('#div2').show();
+    });
+
+    $('#showdiv3').click(function() {
+        $('div[id^=div]').hide();
+        $('#div3').show();
+    });
+
+    $('#showdiv4').click(function() {
+        $('div[id^=div]').hide();
+        $('#div4').show();
+    });
+
+    $('#showdiv5').click(function() {
+        $('div[id^=div]').hide();
+        $('#div5').show();
+    });
+
+    $('#showdiv6').click(function() {
+        $('div[id^=div]').hide();
+        $('#div6').show();
+    });
+
+    $('#showdiv7').click(function() {
+        $('div[id^=div]').hide();
+        $('#div7').show();
+    });
+
+    $('#showdiv8').click(function() {
+        $('div[id^=div]').hide();
+        $('#div8').show();
+    });
+
+    $('#showdiv9').click(function() {
+        $('div[id^=div]').hide();
+        $('#div9').show();
+    });
+
+});
