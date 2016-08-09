c32bc9b746cc92577385450a6b3dae60eef0372c
Andrew Malkin and Martin Cartledge <martin@>
2016-08-09 16:08:19 -0700

    fix ALL merge conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -7,64 +7,32 @@ $(document).ready(function() {
         $('div[id^=div]').hide();
         $('#div2').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv3').click(function() {
         $('div[id^=div]').hide();
         $('#div3').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv4').click(function() {
         $('div[id^=div]').hide();
         $('#div4').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv5').click(function() {
         $('div[id^=div]').hide();
         $('#div5').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv6').click(function() {
         $('div[id^=div]').hide();
         $('#div6').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv7').click(function() {
         $('div[id^=div]').hide();
         $('#div7').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv8').click(function() {
         $('div[id^=div]').hide();
         $('#div8').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
     $('#showdiv9').click(function() {
         $('div[id^=div]').hide();
         $('#div9').show();
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> db4d499397fcfc70ff56fd650fdf8871fd68381d
 });
